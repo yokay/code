3825f7fc2bda9727038f3e034c8db0761bb424c5
yokay <yokay886@qq.com>
2025-04-27 23:36:13 +0800

    m

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,12 @@
 import streamlit as st
 
-pg = st.navigation([st.Page("homepage.py", title="Home")],[st.Page("APcalculator.py",title="test")])
-pg.run()
-
 st.write(
     "Copyright © 2025 by [MYTHBIRD](https://www.mythbird.com)"
 )
+
+pg = st.navigation([st.Page("homepage.py", title="Home")],[st.Page("APcalculator.py",title="test")])
+pg.run()
+
+
 
 
