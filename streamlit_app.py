4f68e562d62d203d98576a68ace9c29be29859ff
yokay <yokay886@qq.com>
2025-04-27 23:40:56 +0800

    ch

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,18 @@ st.write(
     "Copyright © 2025 by [MYTHBIRD](https://www.mythbird.com)"
 )
 
-pg = st.navigation([st.Page("homepage.py", title="Home")],[st.Page("APcalculator.py",title="test")])
+pages = {
+    "Your account": [
+        st.Page("create_account.py", title="Create your account"),
+        st.Page("manage_account.py", title="Manage your account"),
+    ],
+    "Resources": [
+        st.Page("learn.py", title="Learn about us"),
+        st.Page("trial.py", title="Try it out"),
+    ],
+}
+
+pg = st.navigation(pages)
 pg.run()
 
 
